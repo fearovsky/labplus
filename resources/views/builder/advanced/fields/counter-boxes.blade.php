73eeadde03fbc7b65a2404f098901f81a7835fe3
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-05-23 22:33:15 +0200

    Counter boxes

---
+++ resources/views/builder/advanced/fields/counter-boxes.blade.php
@@ -0,0 +1,27 @@
+@if (!empty($field['boxes']))
+    <section class="counter-boxes">
+        <div class="container">
+            @if ($field['heading'])
+                <div class="section-title">
+                    <h2 class="section-title__text">
+                        {!! $field['heading'] !!}
+                    </h2>
+                </div>
+            @endif
+
+            <ul class="counter-boxes-grid">
+                @foreach ($field['boxes'] as $item)
+                    <li class="counter-boxes-grid__item">
+                        <h4 class="counter-boxes-grid__title h6">
+                            {!! $item['title'] !!}
+                        </h4>
+
+                        <p class="counter-boxes-grid__content">
+                            {!! $item['content'] !!}
+                        </p>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </section>
+@endif
