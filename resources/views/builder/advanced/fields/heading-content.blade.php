fbda464504bab544699dd1253c2df024c48b7baa
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-05-24 16:55:53 +0200

    Heading content information box

--- resources/views/builder/advanced/fields/heading-content.blade.php
+++ resources/views/builder/advanced/fields/heading-content.blade.php
@@ -8,6 +8,38 @@
             </div>
         @endif
 
+        @if (!empty($field['information']['title']))
+            <div class="information-box heading-content-box">
+                <div class="information-box-content">
+                    <h5 class="information-box-content__title text-subheader alignleft">
+                        {!! $field['information']['title'] !!}
+                    </h5>
+
+                    <p class="information-box-content__text">
+                        {!! $field['information']['content'] !!}
+                    </p>
+
+                    @if ($field['information']['explanation'])
+                        <p class="information-box-content__explanation">
+                            {{ get_svg('resources.images.icon.info', ['class' => 'information-box-content__explanation-icon']) }}
+                            <span class="information-box-content__explanation-text">
+                                {{ $field['information']['explanation'] }}
+                            </span>
+                        </p>
+                    @endif
+                </div>
+
+                @if ($field['information']['link'])
+                    <div class="information-box-button">
+                        <a href="{{ $field['information']['link']['url'] }}"
+                            class="btn btn-primary btn--small">
+                            {{ $field['information']['link']['title'] }}
+                        </a>
+                    </div>
+                @endif
+            </div>
+        @endif
+
         <div class="heading-content__text">
             {!! $field['content'] !!}
         </div>
