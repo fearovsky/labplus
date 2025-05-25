bd836e22f3cdd08a19688bb91f1cfd145c067dbd
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-05-25 16:57:55 +0200

    init calculator

---
+++ resources/views/builder/advanced/fields/calculator.blade.php
@@ -0,0 +1,15 @@
+<section class="calculator">
+    <div class="container">
+        <div class="box-shadow-white calculator-box">
+
+            @if ($field['heading'])
+                <div class="section-title aligncenter">
+                    <h2 class="section-title__text">
+                        {{ $field['heading'] }}
+                    </h2>
+                </div>
+            @endif
+
+        </div>
+    </div>
+</section>
