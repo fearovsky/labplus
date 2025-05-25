5e129e09be72312ed8c5bde272f873170d34900d
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-05-25 13:23:40 +0200

    Add grid boxes list

---
+++ resources/views/builder/advanced/fields/grid-boxes.blade.php
@@ -0,0 +1,27 @@
+<section class="grid-boxes">
+    <div class="container">
+        @if ($field['heading'])
+            <div class="section-title aligncenter">
+                <h2 class="section-title__text">
+                    {{ $field['heading'] }}
+                </h2>
+            </div>
+        @endif
+
+        @if (!empty($field['boxes']))
+            <ul class="grid-boxes-list">
+                @foreach ($field['boxes'] as $box)
+                    <li class="grid-boxes-list__item">
+                        <p class="grid-boxes-list__item-title h2">
+                            {{ $box['title'] }}
+                        </p>
+
+                        <p class="grid-boxes-list__item-content">
+                            {!! $box['content'] !!}
+                        </p>
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+</section>
