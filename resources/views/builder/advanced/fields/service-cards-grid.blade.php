110f073767951bccf1b2121892c24519e56159a1
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-06-07 11:36:57 +0200

    service grid cards version for desktop

--- resources/views/builder/advanced/fields/service-cards-grid.blade.php
+++ resources/views/builder/advanced/fields/service-cards-grid.blade.php
@@ -12,16 +12,27 @@
             <ul class="service-cards-grid__list">
                 @foreach ($field['items'] as $item)
                     <li class="service-cards-grid__item">
-                        <div class="service-card">
-                            @if (!empty($item['image']))
-                                <div class="service-card__image">
-                                    @image($item['image'], 'full', ['class' => 'service-card__image-thumbnail'])
-                                </div>
-                            @endif
-                            <div class="service-card__content">
-                                <h3 class="service-card__title">{{ $item['title'] }}</h3>
-                                <p class="service-card__description">{{ $item['content'] }}</p>
+                        @if (!empty($item['image']))
+                            <div class="service-cards-grid__image">
+                                @image($item['image'], 'full', ['class' => 'service-cards-grid__image-thumbnail'])
                             </div>
+                        @endif
+                        <div class="service-cards-grid__content">
+                            @if ($item['preTitle'])
+                                <p class="service-cards-grid__content-preheading">
+                                    {{ $item['preTitle'] }}
+                                </p>
+                            @endif
+                            <h3 class="service-cards-grid__content-title h4">{{ $item['title'] }}</h3>
+                            <p class="service-cards-grid__content-description">{{ $item['content'] }}</p>
+
+                            @if ($item['link'])
+                                @include('components.link-more-icon', [
+                                    'url' => $item['link']['url'],
+                                    'target' => '_self',
+                                    'title' => $item['link']['title'],
+                                ])
+                            @endif
                         </div>
                     </li>
                 @endforeach
